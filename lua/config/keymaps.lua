8f65bf161b4aa94b3cceba791d262e4963a77572
ggeorgiu <ggeorgiu@uw.co.uk>
2023-11-20 17:28:14 +0200

    Add comment plugin and add keymaps for opening up terminal

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,8 +1,9 @@
 local keymap = vim.keymap
+local opts =  { noremap = true, silent = true}
 
 -- Dir navigation
-keymap.set("n", "<leader>m", ":NvimTreeFocus<CR>", { noremap = true, silent = true })
-keymap.set("n", "<leader>f", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
+keymap.set("n", "<leader>m", ":NvimTreeFocus<CR>", opts) 
+keymap.set("n", "<leader>f", ":NvimTreeToggle<CR>", opts)
 
 -- Pane navigation
 keymap.set("n", "<C-h>", "<C-w>h", opts) -- Navigate Left
@@ -14,4 +15,14 @@ keymap.set("n", "<C-l>", "<C-w>l", opts) -- Navigate Right
 keymap.set("n", "<leader>sv", ":vsplit<CR>", opts) -- Split vertically
 keymap.set("n", "<leader>sh", ":split<CR>", opts) -- Split horizontally
 keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>", opts) -- Toggle minimize 
+keymap.set("n", "<leader>th", ":split | term<CR>", opts) -- Open terminal split horizontaly
+keymap.set("n", "<leader>tv", ":vsplit | term<CR>", opts) -- Open terminal split horizontaly
+
+-- Indenting
+keymap.set("v", "<", "<gv")
+keymap.set("v", ">", ">gv")
+
+-- Comments
+vim.api.nvim_set_keymap("n", "<C-_>", "gcc", { noremap = false })
+vim.api.nvim_set_keymap("v", "<C-_>", "gcc", { noremap = false })
 
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,6 +4,9 @@ local config = function()
 	local telescope = require("telescope")
 	telescope.setup({
 		defaults = {
+      file_ignore_patterns = {
+        ".git/",
+      },
 			mappings = {
 				i = {
 					["<C-j>"] = "move_selection_next",
@@ -36,9 +39,9 @@ return {
 	dependencies = { "nvim-lua/plenary.nvim" },
 	config = config,
 	keys = {
-	  keymap.set("n", "<leader>fk", "<cmd>Telescope keymaps <CR>"),
+	  keymap.set("n", "<leader>fk", "<cmd>Telescope keymaps<CR>"),
 	  keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<CR>"), 
-		keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>"),
+		keymap.set("n", "<leader>ff", ":Telescope find_files<CR>"),
 		keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<CR>"),
 		keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<CR>"),
 	},
